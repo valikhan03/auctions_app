d0425676cfcba504a76c33d6286e35c666c7ae98
valikhan03 <valikhan.eltaev@gmail.com>
2021-11-25 15:52:07 +0600

    auth service

--- auth/usecase/usecase.go
+++ auth/usecase/usecase.go
@@ -6,27 +6,30 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/golang-jwt/jwt"
 )
 
 type AuthUseCase struct {
-	userRepos      auth.UserRepository
-	hashSalt       string
-	signingKey     []byte
-	expireDuration time.Duration
+	userRepos  auth.UserRepository
+	hashSalt   string
+	signingKey []byte
+	expireTime time.Duration
 }
 
 func NewAuthUseCase(
 	user_repos auth.UserRepository,
 	hash_salt string,
 	signing_key []byte,
-	token_tls_seconds time.Duration) *AuthUseCase {
+	tokenTLSSeconds time.Duration) *AuthUseCase {
 
 	return &AuthUseCase{
-		userRepos:      user_repos,
-		hashSalt:       hash_salt,
-		signingKey:     signing_key,
-		expireDuration: time.Second * token_tls_seconds,
+		userRepos:  user_repos,
+		hashSalt:   hash_salt,
+		signingKey: signing_key,
+		expireTime: tokenTLSSeconds * time.Second,
 	}
 }
 
@@ -45,10 +48,64 @@ func (a *AuthUseCase) SignUp(email, firstname, lastname, password string) error 
 	return a.userRepos.CreateUser(user)
 }
 
-func (a *AuthUseCase) ParseToken(accessToken string) (*models.User, error) {
-	return nil, errors.New("")
+type tokenClaims struct {
+	jwt.StandardClaims
+	User_id string `json:"user_id"`
+}
+
+func (a *AuthUseCase) GenerateAuthToken(user_id string) (string, error) {
+
+	claims := tokenClaims{
+		jwt.StandardClaims{
+			Subject:   "authentification",
+			ExpiresAt: int64(time.Now().Add(a.expireTime).Unix()),
+			Issuer:    "auth-service",
+		},
+		user_id,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	signed_str, err := token.SignedString(a.signingKey)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	return signed_str, err
+}
+
+func (a *AuthUseCase) ParseToken(accessToken string) (string, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Invalid signing method")
+		}
+		return []byte(a.signingKey), nil
+	})
+
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(*tokenClaims); ok && token.Valid {
+		return claims.User_id, nil
+	}else{
+		return "", errors.New("Error invalid access token")
+
+	}
 }
 
 func (a *AuthUseCase) SignIn(email, password string) (string, error) {
-	return "", nil
+	user, err := a.userRepos.GetUser(email, password)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New("user not found")
+	}
+
+	accessToken, err := a.GenerateAuthToken(user.Id)
+	if err != nil {
+		log.Println(err)
+		return "", errors.New("Token generation error")
+	}
+	return accessToken, nil
 }
